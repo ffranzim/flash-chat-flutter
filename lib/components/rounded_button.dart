ea9896b8f105d42be63483c836715f56faf144e0
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2020-08-14 20:13:06 -0300

    Secao 15 - Aula 190 - Fim da aula

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -21,6 +21,7 @@ class RoundedButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             label,
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
